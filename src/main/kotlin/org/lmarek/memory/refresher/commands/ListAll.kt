fff0ae459597785c37fec4f543bc55a4483a94ca
lmarek <marek.lucas@gmail.com>
2021-05-29 16:20:16 +0200

    implemented ListAll.kt command

---
+++ src/main/kotlin/org/lmarek/memory/refresher/commands/ListAll.kt
@@ -0,0 +1,39 @@
+package org.lmarek.memory.refresher.commands
+
+import kotlinx.coroutines.runBlocking
+import org.apache.lucene.analysis.standard.StandardAnalyzer
+import org.apache.lucene.index.DirectoryReader
+import org.apache.lucene.index.IndexReader
+import org.apache.lucene.search.IndexSearcher
+import org.apache.lucene.store.FSDirectory
+import org.lmarek.memory.refresher.document.LuceneFindRegisteredPathsService
+import picocli.CommandLine
+import java.io.File
+import java.nio.file.Path
+import java.nio.file.Paths
+import java.util.concurrent.Callable
+
+@CommandLine.Command(name = "list")
+class ListAll : Callable<Int> {
+
+    override fun call(): Int {
+        createIndexReader(Paths.get(getIndexDirectoryPath())).use {
+            val registeredPathsService = LuceneFindRegisteredPathsService(StandardAnalyzer()) { IndexSearcher(it) }
+            runBlocking {
+                val searchResults = registeredPathsService.listAll()
+                for (result in searchResults)
+                    println(result.path)
+            }
+        }
+        return 0
+    }
+
+    private fun createIndexReader(indexDirectory: Path): IndexReader {
+        return DirectoryReader.open(FSDirectory.open(indexDirectory))
+    }
+
+    private fun getIndexDirectoryPath(): String {
+        val homeDir = System.getProperty("user.home")
+        return homeDir + File.separator + ".memory_refresher" + File.separator + "index"
+    }
+}
--- src/main/kotlin/org/lmarek/memory/refresher/commands/Main.kt
+++ src/main/kotlin/org/lmarek/memory/refresher/commands/Main.kt
@@ -3,7 +3,7 @@ package org.lmarek.memory.refresher.commands
 import picocli.CommandLine
 import java.util.concurrent.Callable
 
-@CommandLine.Command(subcommands = [Init::class, Add::class, Refresh::class, Find::class, Remove::class])
+@CommandLine.Command(subcommands = [Init::class, Add::class, Refresh::class, Find::class, Remove::class, ListAll::class])
 class Main : Callable<Int> {
     override fun call(): Int {
         println("Executing main command")
