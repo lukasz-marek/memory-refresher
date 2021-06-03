5a758b6aea89d90762531d8d3d94e00982443a65
lmarek <marek.lucas@gmail.com>
2021-06-03 16:16:35 +0200

    removed wrappers

--- src/main/kotlin/org/lmarek/memory/refresher/lucene/wrapper/IndexReaderProvider.kt
+++
@@ -1,9 +0,0 @@
-package org.lmarek.memory.refresher.lucene.wrapper
-
-import org.apache.lucene.index.IndexReader
-
-class IndexReaderProvider {
-    interface IndexWriterProvider {
-        suspend fun <T> withIndexReader(action: suspend IndexReader.() -> T)
-    }
-}
--- src/main/kotlin/org/lmarek/memory/refresher/lucene/wrapper/IndexWriterProvider.kt
+++
@@ -1,7 +0,0 @@
-package org.lmarek.memory.refresher.lucene.wrapper
-
-import org.apache.lucene.index.IndexWriter
-
-interface IndexWriterProvider {
-    suspend fun <T> withIndexWriter(action: suspend IndexWriter.() -> T)
-}
