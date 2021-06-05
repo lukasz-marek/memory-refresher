51c260d3d1871c1f1e78db6f9ab4413a0ac5912c
lmarek <marek.lucas@gmail.com>
2021-06-05 21:13:10 +0200

    WriteDocumentServiceImpl.kt tested

--- src/main/kotlin/org/lmarek/memory/refresher/document/service/write/WriteDocumentServiceImpl.kt
+++ src/main/kotlin/org/lmarek/memory/refresher/document/service/write/WriteDocumentServiceImpl.kt
@@ -1,6 +1,12 @@
 package org.lmarek.memory.refresher.document.service.write
 
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.consumeAsFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 import org.lmarek.memory.refresher.document.Document
 import org.lmarek.memory.refresher.document.DocumentPath
 import org.lmarek.memory.refresher.document.repository.write.PathsWriteOnlyRepository
@@ -12,18 +18,39 @@ class WriteDocumentServiceImpl(
     private val documentLoader: DocumentLoader
 ) : WriteDocumentService {
     override suspend fun write(path: DocumentPath) {
-        val document = loadFile(path)
-        pathsWriteOnlyRepository.register(document)
+        val document = loadFile(path, OnMissing.THROW)
+        pathsWriteOnlyRepository.register(document!!) // non-null when throws
     }
 
     override suspend fun write(paths: Flow<DocumentPath>, onMissing: OnMissing) {
-        TODO("Not yet implemented")
+        val documents = paths.map { it to loadFile(it, onMissing) }
+        writeChanges(documents)
     }
 
-    private suspend fun loadFile(path: DocumentPath): Document =
+    private suspend fun writeChanges(documents: Flow<Pair<DocumentPath, Document?>>) {
+        coroutineScope {
+            val toRemove = Channel<DocumentPath>()
+            val toSave = Channel<Document>()
+            launch { pathsWriteOnlyRepository.unregister(toRemove.consumeAsFlow()) }
+            launch { pathsWriteOnlyRepository.register(toSave.consumeAsFlow()) }
+            documents.collect { (path, document) ->
+                if (document != null)
+                    toSave.send(document)
+                else
+                    toRemove.send(path)
+            }
+            toRemove.close()
+            toSave.close()
+        }
+    }
+
+    private suspend fun loadFile(path: DocumentPath, onMissing: OnMissing): Document? =
         try {
             documentLoader.load(File(path.value))
         } catch (loaderException: DocumentLoader.DocumentLoaderException) {
-            throw WriteDocumentService.WriteDocumentServiceException(loaderException)
+            when (onMissing) {
+                OnMissing.REMOVE -> null
+                OnMissing.THROW -> throw WriteDocumentService.WriteDocumentServiceException(loaderException)
+            }
         }
 }
--- src/test/kotlin/org/lmarek/memory/refresher/document/service/write/WriteDocumentServiceImplTest.kt
+++ src/test/kotlin/org/lmarek/memory/refresher/document/service/write/WriteDocumentServiceImplTest.kt
@@ -1,8 +1,9 @@
 package org.lmarek.memory.refresher.document.service.write
 
-import io.mockk.coEvery
-import io.mockk.coVerify
-import io.mockk.mockk
+import io.mockk.*
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
@@ -15,8 +16,22 @@ import java.io.File
 
 class WriteDocumentServiceImplTest {
     private val loader = mockk<DocumentLoader>()
-    private val repository = mockk<PathsWriteOnlyRepository>()
+    private val repository = spyk(object : PathsWriteOnlyRepository {
+        override suspend fun register(document: Document) {}
+
+        override suspend fun register(documents: Flow<Document>) {
+            documents.collect()
+        }
+
+        override suspend fun unregister(path: DocumentPath) {}
+
+        override suspend fun unregister(paths: Flow<DocumentPath>) {
+            paths.collect()
+        }
+
+    })
     private val tested = WriteDocumentServiceImpl(repository, loader)
+
 
     @Nested
     inner class TestWriteOne {
@@ -30,13 +45,13 @@ class WriteDocumentServiceImplTest {
             expectThrows<WriteDocumentService.WriteDocumentServiceException> {
                 tested.write(DocumentPath("/some/path"))
             }
+            coVerify { loader.load(File("/some/path")) }
         }
 
         @Test
         fun `should write when document loader returns file content`() = runBlocking<Unit> {
             // given
             coEvery { loader.load(any()) } returns Document(DocumentPath("/some/path"), "some content")
-            coEvery { repository.register(ofType<Document>()) } returns Unit
 
             // when
             tested.write(DocumentPath("/some/path"))
@@ -44,6 +59,46 @@ class WriteDocumentServiceImplTest {
             // then
             coVerify { loader.load(File("/some/path")) }
             coVerify { repository.register(Document(DocumentPath("/some/path"), "some content")) }
+        }
+    }
+
+    @Nested
+    inner class TestWriteMany {
+
+        @Test
+        fun `should throw when document loader throws and onMissing is set to THROW`() = runBlocking<Unit> {
+            // given
+            coEvery { loader.load(File("/some/path1")) } returns Document(DocumentPath("/some/path1"), "some content 1")
+            coEvery { loader.load(File("/some/path2")) } throws DocumentLoader.DocumentLoaderException(Exception())
+            val documents = flowOf(DocumentPath("/some/path1"), DocumentPath("/some/path2"))
+
+            // when // then
+            expectThrows<WriteDocumentService.WriteDocumentServiceException> {
+                tested.write(documents, OnMissing.THROW)
+            }
+            coVerifyAll {
+                loader.load(File("/some/path1"))
+                loader.load(File("/some/path2"))
+            }
+        }
+
+        @Test
+        fun `should remove document when document loader throws and onMissing is set to REMOVE`() = runBlocking<Unit> {
+            // given
+            coEvery { loader.load(File("/some/path1")) } returns Document(DocumentPath("/some/path1"), "some content 1")
+            coEvery { loader.load(File("/some/path2")) } throws DocumentLoader.DocumentLoaderException(Exception())
+            val documents = flowOf(DocumentPath("/some/path1"), DocumentPath("/some/path2"))
+
+            // when
+            tested.write(documents, OnMissing.REMOVE)
+
+            // then
+            coVerifyAll {
+                loader.load(File("/some/path1"))
+                loader.load(File("/some/path2"))
+                repository.unregister(ofType<Flow<DocumentPath>>())
+                repository.register(ofType<Flow<Document>>())
+            }
         }
     }
 }
